def60d7afd00231abc28ba760e5f48109c24b024
AjayShukla <ajay.shukla@blenheimchalcot.com>
2024-01-11 12:12:21 +0530

    Updated Xpath for first 3 scenario lines

--- src/test/java/pages/loginpage.java
+++ src/test/java/pages/loginpage.java
@@ -10,9 +10,9 @@ import java.time.Duration;
 
 public class loginpage extends basepage{
     String hrm_url="https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
-    String username="//input[@class='oxd-input oxd-input--active' and @name='username']";
-    String password="//input[@class='oxd-input oxd-input--active' and @type='password' and @name='password']";
-    String submit="//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button";
+    String username="//input[@name='username']";
+    String password="//input[@name='password']";
+    String submit="//button[text()=' Login ']";
     String dashboard="//*[@id=\"app\"]/div[1]/div[1]/header/div[1]/div[1]/span/h6";
     public void launch()
     {
