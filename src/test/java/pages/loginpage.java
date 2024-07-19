515b866dfdd9991eb111e4a3a5f24f8c206d3d0d
Susmitnyks <106507263+Susmitnyks@users.noreply.github.com>
2024-07-19 12:07:00 +0530

    success

--- src/test/java/pages/loginpage.java
+++ src/test/java/pages/loginpage.java
@@ -45,6 +45,7 @@ public class loginpage extends basepage{
     public void submit_btn()
     {
         click(submit);
+        System.out.println("success");
     }
 
     public void verifyHomepage_login() throws InterruptedException, IOException {
